4ad5209d3da217edda0c438b848b12df108038d7
Maria Paredes <mariaparedes.2198@gmail.com>
2019-11-17 11:03:02 -0400

    servicio

--- app/controllers/CServicio.java
+++ app/controllers/CServicio.java
@@ -9,15 +9,61 @@ import javax.inject.Inject;
 import play.data.Form;
 import play.data.FormFactory;
 import models.Solicitud;
+import models.Servicio;
+import models.Fundacion;
 import io.ebean.*;
 
 public class CServicio extends Controller{
     
-    public Result listado_servicios() {
-        return ok(views.html.servicios.render());
+    private Form<Servicio> servicioForm;
+
+    @Inject
+    public CServicio(FormFactory formFactory) {
+        this.servicioForm = formFactory.form(Servicio.class);
     }
-    
+
+    public Result listado_servicios() {
+        return ok(views.html.servicios.render(Fundacion.buscador.listado(), Servicio.buscador.listado()));
+    }
+    // CREAR Y MODIFICAR SERVICIO
     public Result incl_servicio() {
-        return ok(views.html.incluir_servicio.render());
+        return ok(views.html.incluir_servicio.render(servicioForm));
+    }
+
+    public Result guardarS() {
+        Form<Servicio> boundForm = servicioForm.bindFromRequest();
+        if (boundForm.hasErrors()) { 
+            flash("error", "Por favor ingrese de nuevo los datos del formulario."); 
+            return badRequest(views.html.incluir_servicio.render(boundForm));
+        }
+        Servicio servicio = boundForm.get();
+        if (servicio.getCodServicio() == null){
+            Ebean.save(servicio);
+            flash("success",String.format("El servicio %s ha sido incluido con éxito.", servicio.getNombre()));
+        } else {
+            Ebean.update(servicio);
+            flash("success",String.format("El servicio %s ha sido modificado con éxito.", servicio.getNombre()));
+        }
+        return redirect(routes.CServicio.listado_servicios());
+    }
+
+    // CARGA LOS DATOS PARA MODIFICARLOS
+    public Result modificarS(UUID codServicio) {
+        final Servicio servicio = Servicio.buscador.porCodigo(codServicio);
+        if (servicio == null) {
+            return notFound(String.format("El servicio %s no existe.", codServicio));
+        }
+
+        Form<Servicio> filledForm = servicioForm.fill(servicio);
+        return ok(views.html.incluir_servicio.render(filledForm));
+    }
+
+    // ELIMINAR SERVICIO
+    public Result eliminarS(final UUID codigo) {
+        final Servicio servicio = Servicio.buscador.porCodigo(codigo);
+        servicio.setEstatus('I');
+        Ebean.update(servicio);
+        flash("success",String.format("El servicio %s ha sido eliminado con éxito.", servicio.getNombre()));
+        return redirect(routes.CServicio.listado_servicios());
     }
 }
