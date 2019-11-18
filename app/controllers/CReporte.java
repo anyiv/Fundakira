2d3ddd661457f1adda8251e64550df6a10aa86a9
anyiv <anyeliv02@gmail.com>
2019-11-18 00:01:56 -0400

    m

--- app/controllers/CReporte.java
+++ app/controllers/CReporte.java
@@ -6,9 +6,16 @@ import java.util.*;
 import java.lang.ProcessBuilder.Redirect;
 
 import javax.inject.Inject;
+
+import buscadores.BuscadorEmpleado;
 import play.data.Form;
 import play.data.FormFactory;
 import models.Solicitud;
+import models.Beneficiario;
+import models.Empleado;
+import models.Servicio;
+import models.Fundacion;
+import models.buscadores.*;
 import io.ebean.*;
 
 public class CReporte extends Controller{
@@ -17,8 +24,10 @@ public class CReporte extends Controller{
         return ok(views.html.reportes.render());
     }
     
-    public Result rp_solicitantes() {
-        return ok(views.html.reporte_solicitantes.render());
+    public Result rp_solicitantes(UUID codfund) {
+        BuscadorEmpleado be = new BuscadorEmpleado();
+        //List<Empleado> empleados_fundacion = be.
+        return ok(views.html.reporte_solicitantes.render(Fundacion.buscador.listado()));
     }
     
     public Result rp_presupuesto() {
