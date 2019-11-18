b78379b4393b2adb07e6b1ee0d3dab9496a1c9fe
anyi <anyeliv02@gmail.com>
2019-11-18 00:01:12 -0400

    fundacion

---
+++ app/models/buscadores/BuscadorGobernacion.java
@@ -0,0 +1,24 @@
+package buscadores;
+
+import java.util.*;
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlID;
+
+import io.ebean.*;
+import io.ebean.Finder;
+import play.data.format.*;
+import play.data.validation.*;
+import models.Gobernacion;
+
+public class BuscadorGobernacion extends Finder<Sting,Gobernacion> {
+
+    public BuscadorGobernacion() {
+      super(Gobernacion.class);
+    }
+  
+    // Add customer finder methods ...
+  
+    public Gobernacion porCodigo(String codigo) {
+      return query().where().eq("codGobernacion", codigo).findOne();
+    }
+  }
